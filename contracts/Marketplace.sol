9cb07e362862fb6c604e79729dd89cc7cdc818e3
Farlonx <nisrecht@gmail.com>
2022-02-21 15:50:31 +0100

    Adding function for listing an item

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -39,4 +39,33 @@ contract Marketplace {
       listingPrice = 0.01 ether;
    }
 
+
+   function listMarketItem(address nftContractAddress, uint256 tokenId, uint256 price) public payable {
+      require(msg.value == listingPrice, "Must pay for exact amount for listing.");
+      require(price > 0, "Price musr be greater than 0.");
+
+      marketItems[itemCounter] =  MarketItem(itemCounter,
+                                             nftContractAddress,
+                                             tokenId,
+                                             payable(msg.sender),
+                                             address(0),
+                                             price,
+                                             false,
+                                             true);
+
+     IERC721(nftContractAddress).transferFrom(msg.Sender,
+                                              address(this),
+                                              tokenId);
+
+     payabke(owner).transfer(listingPrice);
+
+     emit MarketItemListed(itemCounter,
+                           nftContractAddress,
+                           tokenId,
+                           msg.sender,
+                           address(0),
+                           price);
+
+     itemCounter++;
+
 }
